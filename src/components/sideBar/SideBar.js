a1837341ece8686ae909e2e958c6739353b171dd
Your Name <timon_a@hotmail.com>
2021-07-01 09:42:18 +0200

    subcat in sidebar work

--- src/components/sideBar/SideBar.js
+++ src/components/sideBar/SideBar.js
@@ -2,11 +2,30 @@ import products from '../../data/product.json'
 
 
 export const SideBar = () => {
+
+      
+    
+    // const mainCategories = [...new Set(products.map(x => x.mainCategory))]
+    // console.log(mainCategories);
+
+    const categories = Array.from(new Set(products.map(s => s.mainCategory))).map(mainCategory => {
+        return {
+            mainCat: mainCategory,
+            subCat:  products.map(a => a.mainCategory).subCategory
+            // subCat: [...new Set(products.map(x => x.subCategory))]
+            // products.find(s => s.mainCategory === mainCategory).subCategory
+        }
+    })
+
+    
+
+    console.log(categories);
     return (
         <ul>
-        {products.map((product) => {
+        {categories.map((cat) => {
+            console.log(cat);
             return(
-                <li key={product.productId}>{product.mainCategory}</li>
+                <li key={cat.mainCat}>{cat.mainCat}</li>
             )        })}
         </ul>
     )
