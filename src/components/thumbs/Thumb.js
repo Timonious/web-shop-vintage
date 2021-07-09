51092c72940a2afa0a71bcb44adef2bebe46aa52
Your Name <timon_a@hotmail.com>
2021-07-09 15:37:05 +0200

    thumb component

---
+++ src/components/thumbs/Thumb.js
@@ -0,0 +1,17 @@
+export const Thumb = ({amount, productArray}) => {
+   let list = <ul></ul>
+    const thumbMaker = () => {
+         for (let index = 0; index < amount; index++) {
+     
+  }
+    }
+ 
+   console.log(thumbMaker());
+    return (
+        <ul>
+        {/* // <li key={newestProducts[0].productId}><Link to={`/product/${newestProducts[0].productId}`}>{newestProducts[0].productName}</Link></li>
+        // <li key={newestProducts[1].productId}><Link to={`/product/${newestProducts[1].productId}`}>{newestProducts[1].productName}</Link></li>
+        // <li key={newestProducts[2].productId}><Link to={`/product/${newestProducts[2].productId}`}>{newestProducts[2].productName}</Link></li>   */}
+ </ul>
+    )
+}
